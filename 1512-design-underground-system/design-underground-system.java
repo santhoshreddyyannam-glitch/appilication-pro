9da3ada5f6238baba691863a5c701bfecb1aa402
santhosh reddy <jaswanthreddyyannam@gmail.com>
2026-08-24 11:03:33 +0530

    Time: 108 ms (65.41%) | Memory: 57.2 MB (91.92%) - LeetSync

--- 1512-design-underground-system/design-underground-system.java
+++ 1512-design-underground-system/design-underground-system.java
@@ -2,47 +2,43 @@ import java.util.*;
 
 class UndergroundSystem {
 
-    // Store check-in: id -> (station, time)
-    Map<Integer, Pair> checkInMap;
+    
+    Map<Integer, String> station = new HashMap<>();
+    Map<Integer, Integer> time = new HashMap<>();
 
-    // Store route data: "start-end" -> (totalTime, count)
-    Map<String, int[]> routeMap;
+    Map<String, double[]> routes = new HashMap<>();
 
     public UndergroundSystem() {
-        checkInMap = new HashMap<>();
-        routeMap = new HashMap<>();
     }
-    
-    public void checkIn(int id, String stationName, int t) {
-        checkInMap.put(id, new Pair(stationName, t));
-    }
-    
-    public void checkOut(int id, String stationName, int t) {
-        Pair p = checkInMap.get(id);
-        checkInMap.remove(id);
-        
-        String key = p.station + "-" + stationName;
-        int time = t - p.time;
-        
-        routeMap.putIfAbsent(key, new int[2]);
-        routeMap.get(key)[0] += time;   // total time
-        routeMap.get(key)[1] += 1;      // count
-    }
-    
-    public double getAverageTime(String startStation, String endStation) {
-        String key = startStation + "-" + endStation;
-        int[] data = routeMap.get(key);
-        return (double) data[0] / data[1];
-    }
-}
 
-// Helper class
-class Pair {
-    String station;
-    int time;
-    
-    Pair(String station, int time) {
-        this.station = station;
-        this.time = time;
+    public void checkIn(int id, String stationName, int t) {
+        station.put(id, stationName);
+        time.put(id, t);
+    }
+
+    public void checkOut(int id, String stationName, int t) {
+        String start = station.get(id);
+        int startTime = time.get(id);
+
+        String route = start + "#" + stationName;
+        int travelTime = t - startTime;
+
+        if (!routes.containsKey(route)) {
+            routes.put(route, new double[]{0, 0});
+        }
+
+        routes.get(route)[0] += travelTime;
+        routes.get(route)[1]++;
+
+        station.remove(id);
+        time.remove(id);
+    }
+
+    public double getAverageTime(String startStation, String endStation) {
+        String route = startStation + "#" + endStation;
+
+        double[] data = routes.get(route);
+
+        return data[0] / data[1];
     }
 }
